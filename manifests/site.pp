7a17adf84874acccc110bafd3af2089a1f2c88f8
Yufang <yufang@128-223-223-194-wireless.uoregon.edu>
2015-07-08 14:29:59 -0700

    auto updates

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,16 @@
+#matt's
 node ip-10-0-6-70 {
 }
 
+#yufang's
 node ip-10-0-6-127 {
 }
 
+#puppet master
 node ip-10-0-6-254 {
+	cron { "puppet update":
+	   command => "cd /etc/puppet && git pull -q origin master",
+	   user => root,
+	   minute => "*/5",
+	}
 }
