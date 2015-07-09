ddcba8353175c4fcff561020fa6b2a2e594cbdb9
Yufang <yufang@128-223-223-194-wireless.uoregon.edu>
2015-07-09 10:49:56 -0700

    udpate site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,8 +9,10 @@ node ip-10-0-6-127 {
 #puppet master
 node ip-10-0-6-254 {
 	cron { "puppet update":
-	   command => "cd /etc/puppet && git pull -q origin master",
-	   user => root,
-	   minute => "*/5",
+	   	command => "cd /etc/puppet && git pull -q origin master",
+	   	user => root,
+	   	minute => "*/5",
 	}
+
+	include sshd
 }
