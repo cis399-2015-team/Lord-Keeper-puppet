cbfc37279a4d2dabd9b1c4f93572530c36f8e779
Yufang <yufang@128-223-223-194-wireless.uoregon.edu>
2015-07-09 11:38:26 -0700

    udapte init.pp test

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -28,11 +28,15 @@ class sshd {
 
 	ssh_authorized_key{"matt-key-pair":
 		user	=> "ec2",
+		ensure	=> "present",
+		type 	=> "ssh-rsa",
 		key 	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQCAonagiOlt3xIH6pW/ym9Q2b8C8uhF+Vnhe7lPtLThPSqpMpRX9cg/MQsfcXFC49z/hphnjPdbQQc8kGc3pCJyVI7nHY04Th6RBDyCpWqhs8MIZIR3/gG55yUJczd4yQgjHex02F0Jrgtod1HTKG+dY9ZUhI2eqqytO1N5Jj4D6wya/VJ5DjJqFbwn6AkgJKzQeHGY00+mKJiSlzEYNr5Iy8b8DwhScaE9izxNrVSlxhaio5LZagEEyeWzubFQI8Vz3m2nItfAbKAYVl6MHhebfq01HRcCyWpTQxw6FiiR+2k18I+NzYgI/ZDfnT9UXeGgwMXl+6Ywrf0sFFh3NbIL",
 	}
 
 	ssh_authorized_key {'yufang-key-pair":
 		user	=> "ec2", 
+		ensure	=> "present",
+		type	=> "ssh-rsa",
 		key	=> "AAAAB3NzaC1yc2EAAAADAQABAAABAQCkFKYXKYCzmUxUENsGMpILBXWfflS3F4td6QZ/QODxJB6Avj4Np+Lv9SD5a/+AmrCq74Mqbn3C9Z2MCPQX/GLptazGb4f8okOE1guz10ZtTxPbVTU4qxAowu5/GPpGHB+QKRm9IduTWGJLS/bmEoiafvJt/dniWOmwb9/BMctFZ7HyM0DB9h1N4pBbJepXuD3SG/jIPPlgDEuV+Jc5aFM/rjd+YK6Kgp/GEznT/6V3T6kXV1NlEnXm/V2224g/6UlwVaY6bf+p6BFz6+CZSc",
 	}
 }
