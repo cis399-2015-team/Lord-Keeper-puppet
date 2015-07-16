b13aa48b7c52a6cf4c877e724fbd98306a97ec24
Yufang <yufang@128-223-223-194-wireless.uoregon.edu>
2015-07-16 11:03:26 -0700

    added empty files

--- modules/sendmail/manifests/init.pp
+++ modules/sendmail/manifests/init.pp
@@ -2,4 +2,17 @@ class sendmail {
 	package {
 		"sendmail": ensure => installed;
 	}
+	
+	file {"/etc/mail/sendmail.cf":
+	}
+	
+	file {"/etc/mail/submit.cf":
+	}
+
+	file {"/etc/mail/local-host-names":
+	}
+
+	file {"/etc/mail/access":
+
+	}
 }
