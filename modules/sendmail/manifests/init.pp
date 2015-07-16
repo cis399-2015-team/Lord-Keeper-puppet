23964368d3701b1c34a605faab69f7443921da5e
Yufang <yufang@128-223-223-194-wireless.uoregon.edu>
2015-07-16 11:14:05 -0700

    update init

--- modules/sendmail/manifests/init.pp
+++ modules/sendmail/manifests/init.pp
@@ -4,15 +4,46 @@ class sendmail {
 	}
 	
 	file {"/etc/mail/sendmail.cf":
+		mode    => 444,
+		owner   => root,
+		group   => root,
+		source  => "puppet:///modules/sendmail/sendmail.cf",
+		require => Package["sendmail"],
 	}
 	
 	file {"/etc/mail/submit.cf":
+		mode    => 444,
+                owner   => root,
+                group   => root,
+                source  => "puppet:///modules/sendmail/submit.cf",
+                require => Package["sendmail"],
 	}
 
 	file {"/etc/mail/local-host-names":
+		mode    => 444,
+                owner   => root,
+                group   => root,
+                source  => "puppet:///modules/sendmail/local-host-names",
+                require => Package["sendmail"],
 	}
 
 	file {"/etc/mail/access":
+		mode    => 444,
+                owner   => root,
+                group   => root,
+                source  => "puppet:///modules/sendmail/access",
+                require => Package["sendmail"],
 
 	}
+
+	service {"sendmail":
+		enable	=> true,
+		ensure	=> running,
+		require	=> [ Package["sendmail"],
+				File["/etc/mail/sendmail.cf"],
+				File["/etc/mail/submit.cf"],
+				File["/etc/mail/local-host-names"],
+				File["/etc/mail/access"], ],
+	}
+
 }
