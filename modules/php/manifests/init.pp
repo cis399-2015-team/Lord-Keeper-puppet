a24c2806226ac5064d8d8f0ba8f5a2d91b56a1c2
fsmattr <matt@follow.net>
2015-08-14 09:53:35 -0700

    modified php module for security file

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -14,4 +14,12 @@ class php {
 		source  => "puppet:///modules/php/php.ini",
 		require => Package["php"],
 	}
+
+	file { "/etc/php.d/security.ini":
+	mode    => 444,
+	owner   => root,
+	group   => root,
+	source  => "puppet:///modules/php/security.ini",
+	require => Package["php"],
+	}
 }
